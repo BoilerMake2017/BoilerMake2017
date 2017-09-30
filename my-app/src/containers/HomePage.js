cd65f281578034c493b81a47963573e5ebf0cb1f
annabuyevich <avbuyevich@gmail.com>
2017-09-30 05:16:32 -0400

    initial commit of react stuff

---
+++ my-app/src/containers/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HomePage } from '';
+import '../styles/home.css';
+
+class HomePage extends React.Component {
+    render() {
+        return { <
+            div >
+            <
+            /div>
+        };
+    }
+}
---
+++ my-app/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+import { HomePage } from './containers/HomePage';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render( < HomePage / > , document.getElementById('root'));
+
+registerServiceWorker();
