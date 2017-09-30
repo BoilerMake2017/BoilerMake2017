57f8f66006d1380d3d7d920697e97cf90b333f90
praj1812 <prajakti18@gmail.com>
2017-09-30 17:25:03 -0400

    fixed splitFile funcion so fixed csv file formatting

--- my-app/src/splitFile.py
+++ my-app/src/splitFile.py
@@ -7,16 +7,15 @@ def split_file(file_name):
         reader = csv.reader(file)
         for row_num, row in enumerate(reader, 1):
             row_str = (','.join(map(str, row)))
-
             row_str = row_str.replace('\t', ',')
-            row_str = row_str.replace('  ', ',')
-            row_str = row_str.replace(' ', ',')
             if row_num % 2 != 0:
                 last_line_1 = open('cad_1.csv', 'a')
                 last_line_1.write(row_str + "\n")
             else:
                 last_line_2 = open('cad_2.csv', 'a')
                 last_line_2.write(row_str + "\n")
+            # if row_num >= 10:
+            #     break
 
 
 split_file("cad.csv")
